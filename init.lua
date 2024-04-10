2841449ca32c5f25b93f0beb740726925525a499
belst <git@bel.st>
2024-04-10 12:21:28 +0200

    some custom keybinds

--- init.lua
+++ init.lua
@@ -186,7 +186,6 @@ require('lazy').setup({
   --
   --  This is equivalent to:
   -- require('Comment').setup({})
-
   -- "gc" to comment visual regions/lines
   --  { 'numToStr/Comment.nvim', opts = {} },
 
